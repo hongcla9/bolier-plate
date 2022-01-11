5123167479156c0b89e67ac1056e869aa9b0581d
hongcla9 <hongcla9@gmail.com>
2022-01-11 18:05:31 +0900

    비밀정보보호 (mongodb url 이동)

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const {User} = require('./models/User');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://clara:1234@frontminiproject.aqqy1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
     useNewUrlParser : true, useUnifiedTopology: true 
 }).then(()=>console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -11,7 +16,17 @@ mongoose.connect('mongodb+srv://clara:1234@frontminiproject.aqqy1.mongodb.net/my
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.post('/register',(req,res)=>{
+  //회원가입할때 필요한 정보들을 client에서 가져오면 
+  //그것들을 데이터 베이스에 넣어준다.
+const user = new User(req.body);
+user.save((err,userInfo)=>{
+  if(err) return res.json({success:false , err })
+  return res.status(200).json({
+    success:true 
+  })
+})
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
